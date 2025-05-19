935044e90d2368dcd62e1e498a5af138e09ffa71
General-of-Light <aaronjoaquinbasilio@gmail.com>
2025-05-19 20:48:48 +0800

    updated

--- post/src/main/java/com/basilio/post/PostController.java
+++ post/src/main/java/com/basilio/post/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://final-api-pf8l.onrender.com")
 @RequestMapping("/basilio/posts")
 public class PostController {
 
